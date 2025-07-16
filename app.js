9a6900c49fe6b6ca6c49c85682f7b066bbd80a59
Alef <aleftorresbr@gmail.com>
2025-07-15 22:17:03 -0300

    Ajustando conteudo escrito dos cards e ajustando botoes

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ async function consultarAPI() {
   const nomeFilme = inputFilmeNome.value.trim().replace(/\s+/g, '+')
   const URL = `https://www.omdbapi.com/?apikey=909dc5cb&t=${nomeFilme}`
 
-  let ca = ""
-
   try {
     const resp = await fetch(URL)
     const dados = await resp.json() //essa constante contem todos os dados do filme pesquisado
@@ -26,6 +24,7 @@ async function consultarAPI() {
 
     if (filmeExiste) {
       alert("Esse filme já foi pesquisado anteriormente")
+      inputFilmeNome.value = ''
       return
     }
 
@@ -34,45 +33,59 @@ async function consultarAPI() {
 
     const cardFilme = document.createElement('div')
     cardFilme.className = 'card-filme'
-    cardFilme.innerHTML = ` 
-      <img src="${dados.Poster}" alt="Pôster de ${dados.Title}">
-      <h2><strong>${dados.Title}</strong></h2>
-      <p><strong>${dados.Year}</strong></p>
-      <p><strong>Gênero:</strong> ${dados.Genre}</p>
-      <p><strong>Lançamento:</strong> ${dados.Released}</p>
-      <p><strong>Duração:</strong> ${dados.Runtime}</p>
-      <p><strong>Nota IMDb:</strong> ${dados.imdbRating}</p>
-      <button class="btn-detalhes">Mais detalhes</button>
-    `
-    const btnRemover = document.createElement('button');
+    cardFilme.innerHTML = `<img src="${dados.Poster}" alt="Pôster de ${dados.Title}">`
+
+    const cardFilmeConteudo = document.createElement('div')
+    cardFilmeConteudo.className = 'card-filme-conteudo'
+
+    const btnDetalhes = document.createElement('button')
+    btnDetalhes.className = 'btn-detalhes'
+    btnDetalhes.textContent = 'Mais detalhes'
+
+    const btnRemover = document.createElement('button')
     btnRemover.className = 'botao-remover'
-    btnRemover.textContent = 'X';
+    btnRemover.innerHTML = '<img src="x.png" alt=""></img>'
     btnRemover.addEventListener('click', apagarConteudo)
+
     function apagarConteudo() {
       filmes = filmes.filter(filme => filme.Title !== dados.Title)
-      cardFilme.remove();
+      cardFilme.remove()
     }
 
-    cardFilme.appendChild(btnRemover)
+    const btnsContainer = document.createElement('div')
+    btnsContainer.className = 'card-filme-conteudo-btns'
+    btnsContainer.appendChild(btnDetalhes)
+    btnsContainer.appendChild(btnRemover)
 
+    cardFilmeConteudo.innerHTML = `
+      <h2>${dados.Title}</h2>
+      <span>Ano: ${dados.Year}</span>
+      <span>Gênero: ${dados.Genre}</span>
+      <span>Lançamento: ${dados.Released}</span>
+      <span>Duração: ${dados.Runtime}</span>
+      <span>Crítica: ${dados.imdbRating}</span>
+`
+
+    cardFilmeConteudo.appendChild(btnsContainer)
+
+    cardFilme.appendChild(cardFilmeConteudo)
     cartazesFilmes.appendChild(cardFilme)
 
-    // Evento do botão "Mais detalhes"
-    const btnDetalhes = cardFilme.querySelector('.btn-detalhes')
     btnDetalhes.addEventListener('click', () => {
       modalBody.innerHTML = `
-        <h2>${dados.Title}</h2>
-        <p><strong>Diretor:</strong> ${dados.Director}</p>
-        <p><strong>Roteiro:</strong> ${dados.Writer}</p>
-        <p><strong>Atores:</strong> ${dados.Actors}</p>
-        <p><strong>Idioma:</strong> ${dados.Language}</p>
-        <p><strong>País:</strong> ${dados.Country}</p>
-        <p><strong>Votos:</strong> ${dados.imdbVotes}</p>
-        <p><strong>Premiações:</strong> ${dados.Awards}</p>
-        <p><strong>Sinopse:</strong>${dados.Plot}</p>
-      `
+    <h2>${dados.Title}</h2>
+    <p><strong>Diretor:</strong> ${dados.Director}</p>
+    <p><strong>Roteiro:</strong> ${dados.Writer}</p>
+    <p><strong>Atores:</strong> ${dados.Actors}</p>
+    <p><strong>Idioma:</strong> ${dados.Language}</p>
+    <p><strong>País:</strong> ${dados.Country}</p>
+    <p><strong>Votos:</strong> ${dados.imdbVotes}</p>
+    <p><strong>Premiações:</strong> ${dados.Awards}</p>
+    <p><strong>Sinopse:</strong> ${dados.Plot}</p>
+  `
       modal.classList.remove('hidden')
     })
+
 
   } catch (error) {
     console.log('Erro ao consultar API:', error)
@@ -81,12 +94,10 @@ async function consultarAPI() {
   inputFilmeNome.value = ''
 }
 
-// Fechar modal
 fecharModal.addEventListener('click', () => {
   modal.classList.add('hidden')
 })
 
-// Fechar clicando fora do modal
 modal.addEventListener('click', (e) => {
   if (e.target === modal) {
     modal.classList.add('hidden')
